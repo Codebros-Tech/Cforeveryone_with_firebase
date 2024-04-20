0b0d4dcabae1f30aa0896f837aea29c72db54bd7
Funwi Kelsea <funwikelseandohnwi@gmail.com>
2024-04-20 01:22:59 +0100

    Removed some unnecessary files, take steps to switching test from jest to vitest

--- src/views/Pages/NotFound.test.js
+++
@@ -1,9 +0,0 @@
-import {render, screen} from "@testing-library/react";
-import NotFound from './NotFound';
-
-
-test("Renders Private layout", () => {
-    render(<NotFound />)
-    const linkElement = screen.getByText(/404/i);
-    expect(linkElement).toBeInTheDocument();
-});
